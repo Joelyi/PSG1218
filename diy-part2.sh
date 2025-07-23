05b9fb6fafb3f754178da10067791622889c8f49
Joelyi <37099326+Joelyi@users.noreply.github.com>
2025-07-23 10:04:37 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -22,3 +22,6 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 
 # 修改自带默认主题
 #sed -i 's/bootstrap/netgear/g' feeds/luci/collections/luci/Makefile
+
+#修改默认时间格式
+sed -i 's/os.date()/os.date("%Y-%m-%d %H:%M:%S %A")/g' $(find ./package/*/autocore/files/ -type f -name "index.htm")
